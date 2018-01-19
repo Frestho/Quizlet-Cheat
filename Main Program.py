3703f65ab1f38ecfd61adcbd42a57184adbe9903
Conics <34761944+Frestho@users.noreply.github.com>
2018-01-19 10:13:04 -0800

    Cleaned out code

--- Main Program.py
+++ Main Program.py
@@ -1,7 +1,6 @@
 import keyboard, time
 Ptime = time.time()
-words = input('Enter all the words here, each separated by a single space. Stop the program by holding down ESC. Keystrokes will be made 3 seconds after you submit your input so be ready: ')
-words = words.split()
+words = input('Enter all the words here, each separated by a single space. Stop the program by holding down ESC. Keystrokes will be made 3 seconds after you submit your input so be ready: ').split()
 for i in [3, 2, 1]:
     print(i)
     time.sleep(1)
@@ -10,5 +9,4 @@ while time.time()-Ptime < 3600 and not keyboard.is_pressed('esc'):
         keyboard.write(words[i])
         keyboard.press_and_release('enter')
         time.sleep(0.1)#or else your comp will die
-    #j += 1 #lmao why doesn't python support ++
     time.sleep(0.5)#you want your comp to be running, not frozen or crashed    
